fa24476e3cd9e1dab893e50224655c24f76656b4
diditwp <diditwahyupradipta@gmail.com>
2023-03-23 10:02:43 -0500

    Create class_2_testing.py
    Adding some math code to the existing class_1_testing and save as class_2_testing.py file.

---
+++ class_2_testing.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: cclapp
+"""
+
+print("Hello, World!!!")
+
+print("No more math.")
+
+print("But I will add some math again")
+
+def area_of_triangle(base, height):
+    area = base * height * 0.5
+    return area
+
+print(area_of_triangle(10, 2))
+
+a_list = [5, 6, 7, 8, 9]
+
+for i in a_list:
+    square = i ** 2
+    print(square)
+
