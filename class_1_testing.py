5aba64a3b4b745c769352b8c8473d8ef19d87940
diditwp <diditwahyupradipta@gmail.com>
2023-03-30 09:13:37 -0500

    Update class_1_testing.py

--- class_1_testing.py
+++ class_1_testing.py
@@ -9,3 +9,17 @@ print("Hello, World!!!")
 
 print("No more math.")
 
+print("But I will add some math again")
+
+def area_of_triangle(base, height):
+    area = base * height * 0.5
+    return area
+
+print(area_of_triangle(10, 2))
+
+a_list = [5, 6, 7, 8, 9]
+
+for i in a_list:
+    square = i ** 2
+    print(square)
+
